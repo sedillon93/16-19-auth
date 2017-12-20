1cebfd5eb825360bef869dcccad0eb419ee2c573
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-19 17:54:23 -0800

    write account.js file for creating a user and making the account schema with verifyPassword and createToken methods

---
+++ lab-shannon/route/auth-router.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const {Router} = require(`express`);
+const httpErrors = require(`http-errors`);
+const jsonParser = require(`body-parser`).json();
+
+const authRouter = module.exports = new Router();
+
+authRouter.post(`/signup`, (request, response, next) => {
+  if(!request.body.username || !request.body.email || !request.body.password){
+    throw new httpErrors(400, `Username, email, and password are required in order to create an account`);
+  }
+
+
+});
