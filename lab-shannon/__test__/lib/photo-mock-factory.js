76da0bea8f579d337ccb0bb53079d1a7f4e1de99
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-21 16:13:32 -0800

    write photo mock factory file

--- lab-shannon/__test__/lib/photo-mock-factory.js
+++ lab-shannon/__test__/lib/photo-mock-factory.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const faker = require(`faker`);
+const accountMockFactory = require(`./account-mock-factory`);
+const Photo = require(`../../model/photo`);
+
+const photoMockFactory = module.exports = {};
+
+photoMockFactory.create = () => {
+  let mock = {};
+
+  return accountMockFactory.create()
+    .then(accountMock => {
+      mock.account = accountMock;
+      return new Photo({
+        private: faker.random.boolean(),
+        people: faker.lorem.words(5).split(' '),
+        account: accountMock.account._id,
+        url: faker.random.url(),
+      }).save();
+    })
+    .then(photo => {
+      mock.photo = photo;
+      return mock;
+    });
+};
+
+photoMockFactory.remove = () => {
+  return Promise.all([
+    accountMockFactory.remove(),
+    Photo.remove({}),
+  ]);
+};
--- lab-shannon/__test__/photo-router.test.js
+++ lab-shannon/__test__/photo-router.test.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const superagent = require(`superagent`);
+const server = require(`../lib/server`);
+const accountMockFactory = require(`./lib/account-mock-factory`);
+const photoMockFactory = require(`./lib/photo-mock-factory`);
+
+const apiUrl = `http://localhost:${process.env.PORt}`;
+
+describe(`Photo router`, () => {
+  beforeAll(server.start);
+  afterAll(server.stop);
+  afterEach();
+
+  describe(`POST /photos`, () => {
+    test()
+  })
+})
